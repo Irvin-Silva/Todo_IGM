6932c86d05f25fd05f00f27ef803234a2bd448c9
IrvinSilva <64101694+sabi-tech@users.noreply.github.com>
2024-10-01 17:23:35 -0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +8,9 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            build: {
+                manifest: true, 
+            },
         }),
     ],
 });
