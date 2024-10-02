ae1e88f41230994f2166b0a37e005654ed06a5e0
IrvinSilva <64101694+sabi-tech@users.noreply.github.com>
2024-10-02 09:58:34 -0500

    fix buf tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,8 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        
-        <!-- Styles -->
-        <style>
-          
-        </style>
-
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
-    </head>
-    <body class="antialiased">
-        <div id="app">
-            <App />
-        </div>
-        <script src="https://cdn.tailwindcss.com"></script>
-        @vite('resources/js/app.js')
-    </body>
+<html>
+<body>
+  <div id="app"></div>
+  <script src="https://cdn.tailwindcss.com"></script>
+  @vite('resources/js/app.js')
+</body>
 </html>
